839e5d3949aacea9ab40a6e5d92fdaf0dc7d48e6
Kostadin Seymenski <kostadin.seymenski@kodar.net>
2022-03-12 22:58:00 +0200

    Add RoleRepository Interface for persisting and accessing data.

---
+++ SpringStoreAPI/src/main/java/com/springstore/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.springstore.repository;
+import com.springstore.models.ERole;
+import com.springstore.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
